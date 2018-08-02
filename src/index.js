b05a807ef97349ce7f3ac8ea2b74cd8ac1b8670d
Michael Brink <michael.brink@hotmail.com>
2018-08-02 11:54:16 +1000

    End of section 8

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {  BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 
 const rootEl = document.getElementById('root')
 
-let render =() => {
+let render = () => {
   ReactDOM.render(
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>, 
-  rootEl
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
   );
 };
 
@@ -28,6 +31,6 @@ if (module.hot) {
     setTimeout(render);
   });
 }
- render();
+render();
 
 registerServiceWorker();
